37e3d6695767d6342c79f061623b62720da41f71
ErickZH <zaraterick@outlook.com>
2018-07-30 17:13:20 -0500

    Primeros tests

---
+++ tests/Feature/WelcomeUsersTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WelcomeUsersTest extends TestCase
+{
+    /** @test */
+    function it_welcomes_users_with_nickname()
+    {
+        $this->get('/saludo/erick/zaraterick')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido Erick, tu apodo es zaraterick');
+    }
+
+    /** @test */
+    function it_welcomes_users_without_nickname()
+    {
+        $this->get('/saludo/erick')
+            ->assertStatus(200)
+            ->assertSee('Bienvenido Erick');
+    }
+}
